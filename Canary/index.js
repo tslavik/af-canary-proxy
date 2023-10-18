2b8a46d0bb197131d7a0108d8fc03d2c344da67f
TOMAS SLAVIK <toslavik@microsoft.com>
2023-10-18 10:56:05 +0200

    improve the routing

--- Canary/index.js
+++ Canary/index.js
@@ -1,11 +1,18 @@
 const http = require("http")
 
-module.exports = async function (context, req) {
+module.exports = async function (context, req, route) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    var restOfPath = context.bindingData.restOfPath ? context.bindingData.restOfPath : '';
+    // const url = new URL(req.url)
+    // const parts = url.pathname.split('/')
+    // const firstPart = parts.length > 1 ? parts[1] : ''
+    // const restOfPath = parts.slice(2).join('/')
     
-    const url = new URL(req.url)
-    context.log(url.pathname);
-    const responseFromProxy = await fetch("https://purple-meadow-02b644503.3.azurestaticapps.net" + url.pathname, {headers: {'Cookie': 'StaticWebAppsTrafficSplittingCookie=2'}})
+    // context.log('First part:', firstPart);
+    // context.log('Rest of path:', restOfPath);
+
+    context.log(restOfPath);
+    const responseFromProxy = await fetch("https://purple-meadow-02b644503.3.azurestaticapps.net/" + restOfPath, {headers: {'Cookie': 'StaticWebAppsTrafficSplittingCookie=2'}})
     const responseBody = await responseFromProxy.blob()
     const body = await responseBody.text();
     context.log(responseFromProxy.headers.get("Set-Cookie"));
